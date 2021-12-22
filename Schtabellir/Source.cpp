d47a41702b0c44a46fe5db35c79c23e74c473a68
HukakHePak <valsidalvonilb@outlook.com>
2021-12-23 00:19:44 +0400

    add: console render, console control, shoulders draw

--- Schtabellir/Source.cpp
+++ Schtabellir/Source.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <Windows.h>
+#include <conio.h>
+#include "../../../ceerte.lib/ceerte.h"
 
 using namespace std;
 
@@ -8,7 +11,7 @@ class Schtabellir
 {
 	struct position
 	{
-		int x, y;
+		double x, y;
 		double angle;
 	};
 
@@ -17,10 +20,17 @@ class Schtabellir
 		int r, g, b;
 	};
 
-	unsigned int main_angle = 0;
+	struct size
+	{
+		int width, height;
+	};
+
 	vector <vector<unsigned int>> frame;
-	position pos;
-	unsigned int size;
+	position pos{ 0, 0 , 0};
+	size robotSize{ 0 ,0 };
+	size safeSize{ 0, 0 };
+	int safePadding = 2;
+
 
 	color getColor(unsigned int value) {
 		union X
@@ -28,22 +38,34 @@ class Schtabellir
 			unsigned long number;
 			unsigned char bytes[4];
 		};
-		X x{value};
-		return {x.bytes[1], x.bytes[2], x.bytes[3] };
+		X x{ value };
+		return { x.bytes[1], x.bytes[2], x.bytes[3] };
 	}
 
-	double getSin() 
+	double getSin(double angle) 
 	{
-		return sin(pos.angle * 3.1415 / 180) * (-1);
+		return sin(angle * 3.1415 / 180) * (-1);
 	}
 
-	double getCos()
+	double getCos(double angle)
 	{
-		return cos(pos.angle * 3.1415 / 180);
+		return cos(angle * 3.1415 / 180);
+	}
+
+	void printPoint(int x, int y, int color) 
+	{
+		GotoXY(x, y);
+		printColor(color);
 	}
 
 public:
-	Schtabellir(vector <vector<unsigned int>> frame) : frame(frame)
+	Schtabellir(vector <vector<unsigned int>> &frame) : frame(frame)
+	{
+		update(frame);
+		safeSize = { (int)frame[0].size() - safePadding - robotSize.width, (int)frame.size() - safePadding - robotSize.width };
+	}
+
+	void update(vector <vector<unsigned int>> &frame)
 	{
 		position left{ 0,0 };
 		position right{ 0,0 };
@@ -53,14 +75,15 @@ public:
 			for (int j = 0; j < frame[i].size(); j++)
 			{
 				color c = getColor(frame[i][j]);
-				if (c.g >= 200) left = { i, j };
-				if (c.b >= 200) right = { i, j };
+				if (c.g >= 200) left = { (double) i, (double) j };
+				if (c.b >= 200) right = { (double) i, (double) j };
 			}
 		}
-		int proX = left.x - right.x;
-		int proY = left.y - right.y;
+		double proX = left.x - right.x;
+		double proY = left.y - right.y;
 		double size = sqrt(pow(proX, 2) + pow(proY, 2));
-		pos = { left.x - (proX) / 2 , left.y - (proY) / 2, asin(proX / size) * 180 / 3.1415 * (-1)};
+		robotSize = { (int) size };
+		pos = { left.x - (proX) / 2 , left.y - (proY) / 2, asin(proX / size) * 180 / 3.1415 * (-1) };
 	}
 
 	void rotate(int relativeAngle) 
@@ -75,13 +98,15 @@ public:
 
 	void move(int milimeters) 
 	{
-		pos.x += floor(milimeters * getCos());
-		pos.y += floor(milimeters * getSin());
+		pos.x += floor(milimeters * getCos(pos.angle));
+		pos.y += floor(milimeters * getSin(pos.angle));
 	}
 	
 	void render()
 	{
-		cout << "x = " << pos.x << endl << " y = " << pos.y << endl << "angle = " << pos.angle << endl;
+		printPoint(pos.x * 2, pos.y, 4);
+		printPoint((pos.x + getCos(pos.angle - 90) * (robotSize.width / 2)) * 2, pos.y + getSin(pos.angle - 90) * (robotSize.width / 2), 1);
+		printPoint((pos.x + getCos(pos.angle + 90) * (robotSize.width / 2)) * 2, pos.y + getSin(pos.angle + 90) * (robotSize.width / 2), 2);
 	}
 
 	~Schtabellir()
@@ -92,31 +117,76 @@ public:
 
 int main() {
 	vector <vector<unsigned int>> frame;
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < 30; i++)
 	{
 		vector<unsigned int> row;
-		for (int j = 0; j < 8; j++)
+		for (int j = 0; j < 30; j++)
 		{
 			row.push_back(0);
 		}
 		frame.push_back(row);
 	}
 
-	frame[2][0] = 0x00FF0000;
-	frame[0][0] = 0xFF000000;
+	//system("cls");
+	SetTextColor(15);
+	for (int i = 0; i < frame.size(); i++)
+	{
+		for (int j = 0; j < frame[i].size(); j++)
+		{
+			//cout << (char)219 << (char)219;
+		}
+		cout << endl;
+	}
+
+	frame[18][1] = 0x00FF0000;
+	frame[1][1] = 0xFF000000;
 	
 	Schtabellir schtabellir(frame);
-	schtabellir.render();
+	//schtabellir.render();
+	////schtabellir.rotate(180);
+	////schtabellir.rotation(0);
+	//schtabellir.move(20);
+	///*schtabellir.render();
 	//schtabellir.rotate(180);
-	schtabellir.rotation(0);
-	schtabellir.move(7);
-	/*schtabellir.render();
-	schtabellir.rotate(180);
-	schtabellir.move(7);
+	//schtabellir.move(7);
+	//schtabellir.render();
+	//schtabellir.rotation(0);
+	//schtabellir.move(7);*/
+	//schtabellir.render();
+	//schtabellir.rotation(0);
+	//schtabellir.move(10);
 	schtabellir.render();
-	schtabellir.rotation(0);
-	schtabellir.move(7);*/
-	schtabellir.render();
+	char key;
+	while (true)
+	{
+		if (_kbhit())				//отслеживает факт нажатия клавиши
+		{
+			key = _getch();			// принимает значения клавиш
+
+			if (key == 27)	break;			// Escape -> выходит из цикла
+
+
+			switch (key)
+			{
+			case 77:
+				schtabellir.rotate(-15);
+				break;
+			case 75:
+				schtabellir.rotate(15);
+				break;
+			case 72:
+				schtabellir.move(2);
+				break;
+			case 80:
+				schtabellir.move(-2);
+				break;
+			}
+			system("cls");
+			schtabellir.render();
+		}
+	}
+
 	//shtabellir.getPosition();
-	system("pause");
+	//system("pause");
+	//_getch();
 }
